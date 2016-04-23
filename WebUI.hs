e09e6a0449ab821bf9b9a21a8507e2f820d4cd80
Tim C. Schroeder <tim@blitzcode.net>
2016-04-23 11:47:03 +0200

    Added light model icons

--- WebUI.hs
+++ WebUI.hs
@@ -7,11 +7,12 @@ module WebUI ( webUIStart
 import Text.Printf
 import Data.Monoid
 import Control.Concurrent.STM
-import Control.Lens hiding ((#), set)
+import Control.Lens hiding ((#), set, (<.>))
 import Control.Monad
 import Control.Monad.IO.Class
 import qualified Graphics.UI.Threepenny as UI
 import Graphics.UI.Threepenny.Core
+import System.FilePath
 
 import Trace
 import HueJSON
@@ -27,11 +28,32 @@ webUIStart lights = do
     let port = 8001
     traceS TLInfo $ "Starting web server on all interfaces, port " <> show port
     liftIO . startGUI
-        defaultConfig { jsPort = Just port
-                      , jsAddr = Just "0.0.0.0" -- All interfaces, not just loopback
-                      , jsLog  = traceB TLInfo
+        defaultConfig { jsPort   = Just port
+                      , jsAddr   = Just "0.0.0.0" -- All interfaces, not just loopback
+                      , jsLog    = traceB TLInfo
+                      , jsStatic = Just "static"
                       }
         $ setup lights
+
+iconFromLM :: LightModel -> FilePath
+iconFromLM lm = basePath </> fn <.> ext
+  where
+    basePath = "static/svg"
+    ext      = "svg"
+    fn       = case lm of LM_HueBulbA19                -> "white_and_color_e27"
+                          LM_HueSpotBR30               -> "br30"
+                          LM_HueSpotGU10               -> "gu10"
+                          LM_HueLightStrips            -> "lightstrip"
+                          LM_HueLivingColorsIris       -> "iris"
+                          LM_HueLivingColorsBloom      -> "bloom"
+                          LM_LivingColorsGen3Iris      -> "iris"
+                          LM_LivingColorsGen3BloomAura -> "bloom"
+                          LM_HueA19Lux                 -> "white_e27"
+                          LM_ColorLightModule          -> "white_and_color_e27"
+                          LM_ColorTemperatureModule    -> "white_e27"
+                          LM_HueGo                     -> "go"
+                          LM_HueLightStripsPlus        -> "lightstrip"
+                          LM_Unknown _                 -> "white_e27"
 
 setup :: TVar [Light] -> Window -> UI ()
 setup lights' window = do
@@ -39,21 +61,26 @@ setup lights' window = do
     void $ return window # set title "Hue Dashboard"
     -- Lights
     lights <- liftIO . atomically $ readTVar lights'
-    let lightTiles =
-            (flip map) lights $ \light ->
-                let desc = printf "%-25s | %-20s | %-22s | %-10s | %-4.1f%% | %-3s\n"
-                                (light ^. lgtName)
-                                (show $ light ^. lgtType)
-                                (show $ light ^. lgtModelID)
-                                ( if   light ^. lgtState . lsReachable
-                                  then "Reachable"
-                                  else "Not Reachable"
-                                  :: String
-                                )
-                                ( (fromIntegral (light ^. lgtState . lsBrightness . non 255) * 100)
-                                  / 255 :: Float
-                                )
-                                (if light ^. lgtState . lsOn then "On" else "Off" :: String)
-                in UI.div #. "light-tile" #+ [string desc]
-    void $ getBody window #+ lightTiles
+    forM_ lights $ \light -> do
+        let desc = printf "%-25s | %-20s | %-22s | %-10s | %-4.1f%% | %-3s\n"
+                        (light ^. lgtName)
+                        (show $ light ^. lgtType)
+                        (show $ light ^. lgtModelID)
+                        ( if   light ^. lgtState . lsReachable
+                          then "Reachable"
+                          else "Not Reachable"
+                          :: String
+                        )
+                        ( (fromIntegral (light ^. lgtState . lsBrightness . non 255) * 100)
+                          / 255 :: Float
+                        )
+                        (if light ^. lgtState . lsOn then "On" else "Off" :: String)
+        void $ getBody window #+
+            [ UI.img & set UI.src (iconFromLM $ light ^. lgtModelID)
+                     & set style  [ ("width", "75px")
+                                  , ("float", "left")
+                                  ]
+            , UI.div #. "light-tile" #+ [string desc]
+            , UI.div & set style [("clear", "both")]
+            ]
 
