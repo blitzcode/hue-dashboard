af9874166b24ff9585872139199336b0089ebe09
Tim C. Schroeder <tim@blitzcode.net>
2016-04-23 22:37:37 +0200

    Minor cleanup

--- WebUI.hs
+++ WebUI.hs
@@ -62,52 +62,53 @@ iconFromLM lm = basePath </> fn <.> ext
 -- http://www.developers.meethue.com/documentation/color-conversions-rgb-xy
 colorFromLight :: Light -> (Float, Float, Float)
 colorFromLight light
-    | hasXY     = let -- XYZ conversion
-                      x = xyX
-                      y = xyY
-                      z = 1 - x - y
-                      yY = 1
-                      xX = (yY / y) * x
-                      zZ = (yY / y) * z
-                      -- sRGB D65 conversion
-                      r_sRGB =  xX * 1.656492 - yY * 0.354851 - zZ * 0.255038;
-                      g_sRGB = -xX * 0.707196 + yY * 1.655397 + zZ * 0.036152;
-                      b_sRGB =  xX * 0.051713 - yY * 0.121364 + zZ * 1.011530;
-                      -- Clamp
-                      (rClamp, gClamp, bClamp)
-                          | r_sRGB > b_sRGB && r_sRGB > g_sRGB && r_sRGB > 1 =
-                                (1, g_sRGB / r_sRGB, b_sRGB / r_sRGB) -- Red is too big
-                          | g_sRGB > b_sRGB && g_sRGB > r_sRGB && g_sRGB > 1 =
-                                (r_sRGB / g_sRGB, 1, b_sRGB / g_sRGB) -- Green is too big
-                          | b_sRGB > r_sRGB && b_sRGB > g_sRGB && b_sRGB > 1 =
-                                (r_sRGB / b_sRGB, g_sRGB / b_sRGB, 1) -- Blue is too big
-                          | otherwise =
-                                (r_sRGB, g_sRGB, b_sRGB)
-                      -- Gamma correction
-                      rGamma | rClamp <= 0.0031308 = 12.92 * rClamp
-                             | otherwise           = (1 + 0.055) * (rClamp ** (1 / 2.4)) - 0.055
-                      gGamma | gClamp <= 0.0031308 = 12.92 * gClamp
-                             | otherwise           = (1 + 0.055) * (gClamp ** (1 / 2.4)) - 0.055
-                      bGamma | bClamp <= 0.0031308 = 12.92 * bClamp
-                             | otherwise           = (1 + 0.055) * (bClamp ** (1 / 2.4)) - 0.055
-                      -- Normalize
-                      (r, g, b) | rGamma > bGamma && rGamma > gGamma =
-                                    -- Red is largest
-                                    if   rGamma > 1
-                                    then (1, gGamma / rGamma, bGamma / rGamma)
-                                    else (rGamma, gGamma, bGamma)
-                                | gGamma > bGamma && gGamma > rGamma =
-                                    -- Green is largest
-                                    if   gGamma > 1
-                                    then (rGamma / gGamma, 1, bGamma / gGamma)
-                                    else (rGamma, gGamma, bGamma)
-                                | bGamma > rGamma && bGamma > gGamma =
-                                    -- Blue is largest
-                                    if   bGamma > 1
-                                    then (rGamma / bGamma, gGamma / bGamma, 1)
-                                    else (rGamma, gGamma, bGamma)
-                                | otherwise = (rGamma, gGamma, bGamma)
-                  in (r, g, b)
+    | hasXY =
+          let -- XYZ conversion
+              x = xyX
+              y = xyY
+              z = 1 - x - y
+              yY = 1
+              xX = (yY / y) * x
+              zZ = (yY / y) * z
+              -- sRGB D65 conversion
+              r_sRGB =  xX * 1.656492 - yY * 0.354851 - zZ * 0.255038;
+              g_sRGB = -xX * 0.707196 + yY * 1.655397 + zZ * 0.036152;
+              b_sRGB =  xX * 0.051713 - yY * 0.121364 + zZ * 1.011530;
+              -- Clamp
+              (rClamp, gClamp, bClamp)
+                  | r_sRGB > b_sRGB && r_sRGB > g_sRGB && r_sRGB > 1 =
+                        (1, g_sRGB / r_sRGB, b_sRGB / r_sRGB) -- Red is too big
+                  | g_sRGB > b_sRGB && g_sRGB > r_sRGB && g_sRGB > 1 =
+                        (r_sRGB / g_sRGB, 1, b_sRGB / g_sRGB) -- Green is too big
+                  | b_sRGB > r_sRGB && b_sRGB > g_sRGB && b_sRGB > 1 =
+                        (r_sRGB / b_sRGB, g_sRGB / b_sRGB, 1) -- Blue is too big
+                  | otherwise =
+                        (r_sRGB, g_sRGB, b_sRGB)
+              -- Gamma correction
+              rGamma | rClamp <= 0.0031308 = 12.92 * rClamp
+                     | otherwise           = (1 + 0.055) * (rClamp ** (1 / 2.4)) - 0.055
+              gGamma | gClamp <= 0.0031308 = 12.92 * gClamp
+                     | otherwise           = (1 + 0.055) * (gClamp ** (1 / 2.4)) - 0.055
+              bGamma | bClamp <= 0.0031308 = 12.92 * bClamp
+                     | otherwise           = (1 + 0.055) * (bClamp ** (1 / 2.4)) - 0.055
+              -- Normalize
+              (r, g, b) | rGamma > bGamma && rGamma > gGamma =
+                            -- Red is largest
+                            if   rGamma > 1
+                            then (1, gGamma / rGamma, bGamma / rGamma)
+                            else (rGamma, gGamma, bGamma)
+                        | gGamma > bGamma && gGamma > rGamma =
+                            -- Green is largest
+                            if   gGamma > 1
+                            then (rGamma / gGamma, 1, bGamma / gGamma)
+                            else (rGamma, gGamma, bGamma)
+                        | bGamma > rGamma && bGamma > gGamma =
+                            -- Blue is largest
+                            if   bGamma > 1
+                            then (rGamma / bGamma, gGamma / bGamma, 1)
+                            else (rGamma, gGamma, bGamma)
+                        | otherwise = (rGamma, gGamma, bGamma)
+          in (r, g, b)
     | otherwise = (255, 255, 255) -- TODO: Handle color temperature, might need
                                   --       to check color mode field as well
   where
@@ -119,79 +120,16 @@ colorFromLight light
 htmlColorFromRGB :: (Float, Float, Float) -> String
 htmlColorFromRGB (r, g, b) = printf "rgb(%.0f, %.0f, %.0f)" (r * 255) (g * 255) (b * 255)
 
-{-
-    float x = xy.x;
-    float y = xy.y;
-    float z = 1.0f - x - y;
-
-    float Y = 1.0f;
-    float X = (Y / y) * x;
-    float Z = (Y / y) * z;
-
-    // sRGB D65 conversion
-    float r =  X * 1.656492f - Y * 0.354851f - Z * 0.255038f;
-    float g = -X * 0.707196f + Y * 1.655397f + Z * 0.036152f;
-    float b =  X * 0.051713f - Y * 0.121364f + Z * 1.011530f;
-
-    if (r > b && r > g && r > 1.0f) {
-        // red is too big
-        r = 1.0f;
-        g = g / r;
-        b = b / r;
-    }
-    else if (g > b && g > r && g > 1.0f) {
-        // green is too big
-        r = r / g;
-        g = 1.0f;
-        b = b / g;
-    }
-    else if (b > r && b > g && b > 1.0f) {
-        // blue is too big
-        r = r / b;
-        g = g / b;
-        b = 1.0f;
-    }
-
-    // Apply gamma correction
-    r = r <= 0.0031308f ? 12.92f * r : (1.0f + 0.055f) * pow(r, (1.0f / 2.4f)) - 0.055f;
-    g = g <= 0.0031308f ? 12.92f * g : (1.0f + 0.055f) * pow(g, (1.0f / 2.4f)) - 0.055f;
-    b = b <= 0.0031308f ? 12.92f * b : (1.0f + 0.055f) * pow(b, (1.0f / 2.4f)) - 0.055f;
-
-    if (r > b && r > g) {
-        // red is biggest
-        if (r > 1.0f) {
-            g = g / r;
-            b = b / r;
-            r = 1.0f;
-        }
-    }
-    else if (g > b && g > r) {
-        // green is biggest
-        if (g > 1.0f) {
-            r = r / g;
-            b = b / g;
-            g = 1.0f;
-        }
-    }
-    else if (b > r && b > g) {
-        // blue is biggest
-        if (b > 1.0f) {
-            r = r / b;
-            g = g / b;
-            b = 1.0f;
-        }
-    }
--}
 setup :: TVar [Light] -> Window -> UI ()
 setup lights' window = do
     -- Title
     void $ return window # set title "Hue Dashboard"
     -- Bootstrap JS, should be in the body
-    --void $ getBody window #+
-    --    [mkElement "script" & set (attr "src") ("static/bootstrap/js/bootstrap.min.js")]
+    -- void $ getBody window #+
+    --     [mkElement "script" & set (attr "src") ("static/bootstrap/js/bootstrap.min.js")]
     -- Lights
     lights <- liftIO . atomically $ readTVar lights'
-    -- TODO: This will throw an exception if we don't have that element...
+    -- TODO: This will throw an exception if we don't have the element...
     void $ (fromJust <$> getElementById window "lights") #+
       [ UI.div #. "thumbnails" #+
           ((flip map) lights $ \light ->
@@ -228,57 +166,4 @@ setup lights' window = do
                 ]
           )
       ]
-
-    return ()
-
-{-
-      <div class="thumbnails">
-        <div class="thumbnail" style="opacity: 1.0;">
-          <div class="light-caption small" style="">Kinderzimmer Tisch</div>
-          <img class="img-rounded" style="background: green;" src="static/svg/br30.svg"/>
-          <div class="text-center">
-            <h6>
-              <small>
-                Hue Bulb A19
-                <br/>
-                Extended Color Light
-              </small>
-            </h6>
-          </div>
-          <div class="small text-center">Brightness</div>
-          <div class="progress">
-            <div class="progress-bar progress-bar-info" role="progressbar" style="width: 25%;">
-              <small>25%</small>
-            </div>
-          </div>
-        </div>
-      </div>
--}
-
-{-
-    lights <- liftIO . atomically $ readTVar lights'
-    lightsElement <- fromJust <$> getElementById window "lights"
-    forM_ lights $ \light -> do
-        let desc = printf "%-25s | %-20s | %-22s | %-10s | %-4.1f%% | %-3s\n"
-                        (light ^. lgtName)
-                        (show $ light ^. lgtType)
-                        (show $ light ^. lgtModelID)
-                        ( if   light ^. lgtState . lsReachable
-                          then "Reachable"
-                          else "Not Reachable"
-                          :: String
-                        )
-                        ( (fromIntegral (light ^. lgtState . lsBrightness . non 255) * 100)
-                          / 255 :: Float
-                        )
-                        (if light ^. lgtState . lsOn then "On" else "Off" :: String)
-        return lightsElement #+
-            [ UI.img & set UI.src (iconFromLM $ light ^. lgtModelID)
-                     & set style  [ ("width", "75px")
-                                  , ("float", "left")
-                                  ]
-            , UI.div #. "light-tile-abc" #+ [string desc]
-            , UI.div & set style [("clear", "both")]
-            ]
--}
 
